35cf423c8293935bb224e5b637fed08be43ad9df
nhoton04 <ndragon2k4@gmail.com>
2025-10-02 23:42:32 +0700

    projects1

---
+++ THN/app/src/main/java/de/hdodenhof/circleimageview/CircleImageViewImpl.java
@@ -0,0 +1,9 @@
+package de.hdodenhof.circleimageview;
+
+import android.content.Context;
+
+public class CircleImageViewImpl extends CircleImageView {
+    public CircleImageViewImpl(Context context) {
+        super(context);
+    }
+}
